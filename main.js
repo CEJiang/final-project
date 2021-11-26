4c9cbb8153e766dacd6c591e7499151ef21d414d
CEJiang <89209085+CEJiang@users.noreply.github.com>
2021-11-26 14:42:32 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var rlele = document.getElementById("lele");
+rlele.addEventListener("click", () => {
+    alert("推廣樂樂");
+})
+var rcurry = document.getElementById("rcurry");
+rcurry.addEventListener("click", () => {
+    alert("推廣鼎咖哩");
+})
+var rchicken = document.getElementById("rchicken");
+rchicken.addEventListener("click", () => {
+    alert("推廣吉味屋");
+})
